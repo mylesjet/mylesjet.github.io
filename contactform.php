600f86601dc2b9e514d0f69bb09816f81bb5d2fd
Michael Mclean <michaelmcleanjs@gmail.com>
2018-06-23 07:46:32 +0930

    Form 2

--- contactform.php
+++ contactform.php
@@ -1,14 +1,11 @@
 <?php 
-
-if (isset($_POST['submit'])) {
     $mailFrom = $_POST['email'];
     $contents = $_POST['contents'];
     $mailTo = "tmester.yassine.n@puds5k7lca9zq.tk";
     $headers = "This email was sent from the Myles Website :)";
+    $subject = "This is an automated email sent from my Email Contact Form.";
     
     
-    mail("tmester.yassine.n@puds5k7lca9zq.tk","Website Contact Form",$contents);
+    mail($mailTo,$subject,$contents);
     header("Location: contact.html?mailsend");
-}
-
 ?>
